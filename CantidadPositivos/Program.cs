e309fa7e5b52b3ed2fd7648027010afefa2aa0ea
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2021-12-19 20:56:31 -0300

    Agregar archivos de proyecto.

---
+++ CantidadPositivos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CantidadPositivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            int ContadorPositivos = 0;
+            for(int i = 1; i <= 20; i++)
+            {
+                Console.Write("Ingrese un Número por favor: ");
+                Num = int.Parse(Console.ReadLine());
+                if(Num > 0)
+                {
+                    ContadorPositivos = ContadorPositivos + 1;
+                }
+            }
+            Console.WriteLine("La cantidad de números positivos ingresados es: {0}", ContadorPositivos);
+            Console.Read();
+        }
+    }
+}
